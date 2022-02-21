1c93e619cea449fca9367e598a23438c3537f3cb
sunhejie <sunhejie@126.com>
2022-02-21 16:15:51 +0800

    chore: 初始化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="user-wrap wrap">
-    <div class="inner"></div>
+    <div class="inner">123</div>
   </div>
 </template>
 <script setup lang="ts"></script>
 <style>
 .wrap {
   @apply border;
+}
+.inner {
+  @apply w-20 h-20 bg-red-300;
 }
 </style>
